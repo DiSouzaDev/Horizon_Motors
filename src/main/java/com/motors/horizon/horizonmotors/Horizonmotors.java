57e3b383b872dfe04f05896ffe147d94034bf4e4
Giovana Alves Magalhães <146298988+alvesmgiovana@users.noreply.github.com>
2024-04-12 21:28:19 -0300

    Entrega01(gui): gui de login sem funcionalidades
    Tela de login sem funcionalidade desenvolvida no NetBeans para Horizon Motors.

---
+++ src/main/java/com/motors/horizon/horizonmotors/Horizonmotors.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.motors.horizon.horizonmotors;
+
+/**
+ *
+ * @author 823214424
+ */
+public class Horizonmotors {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
